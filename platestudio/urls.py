ec6be7ade03139f4b1e4331141c5d51a825f9e64
Ko <eorn21301@gmail.com>
2021-12-23 10:55:59 +0900

    first commit

--- platestudio/urls.py
+++ platestudio/urls.py
@@ -15,12 +15,14 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from django.views.generic import RedirectView
 from shop import views
 from django.conf import settings
 from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', RedirectView.as_view(url = '/shop/', permanent = True)),
     path('shop/', include('shop.urls')),
     path('search/', include('search_app.urls')),
     path('cart/', include('cart.urls')),
