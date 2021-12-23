5df795380530d143cd4f95f10122eda5662a45a6
hyunn00 <90684987+hyunn00@users.noreply.github.com>
2021-12-23 16:32:29 +0900

    Update settings.py

--- platestudio/settings.py
+++ platestudio/settings.py
@@ -22,19 +22,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-secret_file = os.path.join(BASE_DIR, 'secrets.json') #secrets.json을 불러와 줍니다.
-
-with open(secret_file, 'r') as f: #open as로 secret.json을 열어줍니다.
-    secrets = json.loads(f.read())
-
-def get_secret(setting, secrets=secrets): #예외 처리를 통해 오류 발생을 검출합니다.
-    try:
-        return secrets[setting]
-    except KeyError:
-        error_msg = "Set the {} environment variable".format(setting)
-        raise ImproperlyConfigured(error_msg)
-
 SECRET_KEY = get_secret("SECRET_KEY")
+
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
